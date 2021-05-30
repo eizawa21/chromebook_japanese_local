1a2d3623244ce6a777fc52f7a931dd4114040268
eizawa21 <84676021+eizawa21@users.noreply.github.com>
2021-05-30 16:38:52 +0900

    Update japanese_locale.sh
    sudo　入れ忘れた…

--- japanese_locale.sh
+++ japanese_locale.sh
@@ -6,10 +6,10 @@ source /etc/default/locale
 sudo apt -y install fcitx-mozc
 echo "/usr/bin/fcitx-autostart" >> ~/.sommelierrc
 sudo cp /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf.bak
-chmod 777 /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
+sudo chmod 777 /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
 echo Environment='"GTK_IM_MODULE=fcitx"' >> /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
 echo Environment='"QT_IM_MODULE=fcitx"' >> /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
 echo Environment='"XMODIFIERS=@im=fcitx"' >> /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
 echo Environment='"GDK_BACKEND=x11"' >> /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
-chmod 644 /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
+sudo chmod 644 /etc/systemd/user/cros-garcon.service.d/cros-garcon-override.conf
 echo "Japanese input environment setup successful"
